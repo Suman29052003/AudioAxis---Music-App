6a18d3d64e8677cc69cf2fcb9a713162a2509211
SumanSaha <8759127658itisaha@gmail.com>
2024-02-04 23:29:14 +0530

    added bootstrap

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -4,7 +4,7 @@ import homeIcon from "../../IconsAndImages/home.png";
 import searchIcon from "../../IconsAndImages/search.png";
 import radioIcon from "../../IconsAndImages/radio.png";
 import trendingIcon from "../../IconsAndImages/trending.png";
-
+import leftArrowIcon from "../../IconsAndImages/left_icon.png";
 const Main = () => {
   return (
     <div className="main">
@@ -34,7 +34,33 @@ const Main = () => {
         </div>
       </div>
       {/* Right Content Section */}
-      <div className="right"></div>
+      <div className="right">
+        <div className="searchbar">
+          <img src={leftArrowIcon} alt="" />
+          <div className="searchInput flex">
+            <img src={searchIcon} alt="" />
+            <input
+              type="text"
+              placeholder="Search for music, audio or videos..."
+            />
+          </div>
+        </div>
+        <div className="right-main">
+          <div className="card" style="width: 18rem;">
+            <img src="..." className="card-img-top" alt="..." />
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <p className="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+              <a className="btn btn-primary">
+                Go somewhere
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
